e1756b4e9b5f94aa2f226ae983e27a141b1ebf02
Emily Tarrant <emily.tarrant@bigcommerce.com>
2019-06-13 17:31:49 -0500

    Add answer stats for answered question options

--- src/components/AnsweredQuestion.js
+++ src/components/AnsweredQuestion.js
@@ -1,17 +1,54 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class AnsweredQuestion extends Component {
   render() {
-    const { question } = this.props;
+    const { authUser, question, usersCount } = this.props;
+    const { optionOne, optionTwo } = question;
+
+    const optionOneVotes = optionOne.votes.length;
+    const optionTwoVotes = optionTwo.votes.length;
+    const optionOnePercent = Math.floor((optionOneVotes / usersCount) * 100);
+    const optionTwoPercent = Math.floor((optionTwoVotes / usersCount) * 100);
 
     return (
       <div>
-        Would you rather... (you already answered!)
-        <div>{question.optionOne.text}</div>
-        <div>{question.optionTwo.text}</div>
+        <div>
+          <b>{optionOne.text}</b>
+          {" | "}
+          {optionOne.votes.length}{" "}
+          {optionOne.votes.length === 1 ? "vote" : "votes"} ({optionOnePercent}
+          %)
+          {optionOne.votes.includes(authUser) && (
+            <span>
+              {" "}
+              | <b>Your answer!</b>
+            </span>
+          )}
+        </div>
+        <div>
+          <b>{optionTwo.text}</b>
+          {" | "}
+          {optionTwo.votes.length}{" "}
+          {optionTwo.votes.length === 1 ? "vote" : "votes"} ({optionTwoPercent}
+          %)
+          {optionTwo.votes.includes(authUser) && (
+            <span>
+              {" "}
+              | <b>Your answer!</b>
+            </span>
+          )}
+        </div>
       </div>
     );
   }
 }
 
-export default AnsweredQuestion;
+function mapStateToProps({ authUser, users }) {
+  return {
+    authUser,
+    usersCount: Object.keys(users).length
+  };
+}
+
+export default connect(mapStateToProps)(AnsweredQuestion);
