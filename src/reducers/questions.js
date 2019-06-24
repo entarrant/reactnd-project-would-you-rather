7fed9d16e440e44dda0dc12b73667ddd25378116
Emily Tarrant <emily.tarrant@bigcommerce.com>
2019-06-24 12:30:25 -0500

    Sort questions in in descending order by timestamp

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -27,25 +27,33 @@ export default function questions(state = null, action) {
       return {
         ...state,
         allQuestions: questions,
-        unanswered,
-        answered
+        unanswered: unanswered.sort(
+          (q1, q2) => questions[q2].timestamp - questions[q1].timestamp
+        ),
+        answered: answered.sort(
+          (q1, q2) => questions[q2].timestamp - questions[q1].timestamp
+        )
       };
     case CLEAR_QUESTIONS:
       return null;
     case ADD_QUESTION:
       const { question } = action;
+      // Add new question to front of user's unanswered list
+      state.unanswered.unshift(question.id);
 
       return {
         ...state,
         allQuestions: {
           ...state.allQuestions,
           [question.id]: question
-        },
-        unanswered: state.unanswered.concat(question.id)
+        }
       };
     case ANSWER_QUESTION:
       const { qid, answer } = action;
       const quest = state.allQuestions[qid];
+
+      // Add answered question to front of user's answered list
+      state.answered.unshift(quest.id);
 
       return {
         ...state,
@@ -59,8 +67,7 @@ export default function questions(state = null, action) {
             }
           }
         },
-        unanswered: state.unanswered.filter(id => id !== qid),
-        answered: state.answered.concat(qid)
+        unanswered: state.unanswered.filter(id => id !== qid)
       };
     default:
       return state;
